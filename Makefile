fc4ec5fb354c4303009a0677e4a0a451e50139a9
Jake Hughes <jh@jakehughes.uk>
2025-03-19 11:29:12 +0000

    Process results into table

--- Makefile
+++ Makefile
@@ -6,50 +6,81 @@ export PATH := $(PWD)/depot_tools:$(PATH)
 PWD != pwd
 CHR_SRC = $(PWD)/chromium/src
 RUNNER = $(CHR_SRC)/third_party/crossbench/cb.py
+ifeq ($(USE_XVFB),true)
+RUNNER := xvfb-run -a $(RUNNER)
+endif
+PYTHON = python3
+ifeq ($(USE_REMOTEEXEC),true)
+REMOTEEXEC = use_remoteexec=true
+endif
 
 CFGS = $(wildcard $(PWD)/configs/*.gn)
 NINJA_FILES := $(patsubst $(PWD)/configs/%.gn,$(CHR_SRC)/out/%/args.gn,$(CFGS))
 CHROME_TARGETS := $(patsubst $(CHR_SRC)/out/%/args.gn,$(CHR_SRC)/out/%/chrome,$(NINJA_FILES))
 BENCHMARKS = speedometer2.1 jetstream3.0 motionmark1.3
 
-.PHONY: build bench clean
+# BENCHMARKS = speedometer_2.1 jetstream_3.0 motionmark_1.3
+BENCHMARKS = speedometer_2.1
+RESULTS = $(PWD)/results
+RAW = $(addprefix $(RESULTS)/rawdata/$(BENCHMARKS)/, $(addsuffix .json, $(BENCHMARKS)))
+PLOTS = $(addprefix $(RESULTS)/plots/, $(addsuffix .md, $(BENCHMARKS)))
 
-all: build bench
+# Processing results into plots
+PYTHON = python3
+VENV = $(PWD)/venv
+PIP = $(VENV)/bin/pip
+PYTHON_EXEC = $(VENV)/bin/python
 
-bench: $(BENCHMARKS)
+.PHONY: build bench plot clean
 
-build: $(NINJA_FILES) $(CHROME_TARGETS)
+all: plot
 
-ifeq ($(USE_XVFB),true)
-RUNNER := xvfb-run -a $(RUNNER)
-endif
+plot: $(VENV)/bin/activate $(RESULTS)/plots $(PLOTS)
 
-$(BENCHMARKS):
-	$(RUNNER) $@ --env-validation=skip --repeat=$(ITERS) \
+perf: $(CHR_SRC)
+	@if [ $$(id -u) -ne 0 ]; then \
+        echo "Setting CPU frequency governor requires sudo."; \
+        exit 1; \
+    fi
+	sh $(CHR_SRC)/v8/tools/cpu.sh performance
+
+$(RESULTS)/plots:
+	mkdir -p $(RESULTS)/plots
+
+$(VENV)/bin/activate: requirements.txt
+	$(PYTHON) -m venv $(VENV)
+	$(PIP) install -r $<
+
+$(PLOTS): $(RAW)
+	@echo $(PYTHON) process.py $< $@
+	$(PYTHON_EXEC) process.py $< $@
+
+bench: $(RAW)
+
+$(RAW): $(CHROME_TARGETS)
+	$(RUNNER) $(basename $(notdir $(RAW))) --env-validation=skip --repeat=$(ITERS) \
 		$(foreach browser, $(CHROME_TARGETS), \
-		--browser=$(browser))
+		--browser=$(browser)) \
+		--out-dir=$(dir $@)
 
-depot-tools:
-	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+build: perf $(NINJA_FILES) $(CHROME_TARGETS)
 
-$(CHR_SRC): 
+$(NINJA_FILES): $(CFGS)
+	cd $(CHR_SRC) && gn gen --args="$(shell cat $<) $(REMOTEEXEC)" $(dir $@)
+
+$(CHROME_TARGETS): $(NINJA_FILES)
+	autoninja -C $(dir $@) chrome chromedriver
+
+$(CHR_SRC): depot_tools
 	mkdir chromium
 	cd chromium && fetch --no-history chromium
 	cd $@ && gclient runhooks
 
-ifeq ($(USE_REMOTEEXEC),true)
-$(CHR_SRC)/out/%/args.gn: $(PWD)/configs/%.gn
-	cd $(CHR_SRC) && gn gen --args="$(shell cat $<) use_remoteexec=true" $(dir $@)
-else
-$(CHR_SRC)/out/%/args.gn: $(PWD)/configs/%.gn
-	cd $(CHR_SRC) && gn gen --args="$(shell cat $<)" $(dir $@)
-endif
-
-$(CHR_SRC)/out/%/chrome: $(CHR_SRC)/out/%/args.gn
-	autoninja -C $(dir $@) chrome chromedriver
+depot_tools:
+	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
 clean-results:
-	rm -rf $(CHR_SRC)/third_party/crossbench/results
+	rm -rf $(RESULTS)
 
 clean-builds:
 	rm -rf $(CHR_SRC)/out
--- process.py
+++ process.py
@@ -3,66 +3,74 @@ import pandas as pd
 from scipy.stats import t
 from pathlib import Path
 import sys
+import os
+import math
 
-def confidence_interval(stddev, n):
+PRETTY_CFGS = {
+    "handles": "Handles",
+    "handles_no_pc": "Handles (w/o pointer compression)",
+    "direct_refs": "Direct Refs.",
+    "direct_refs_no_pc": "Direct Refs. (w/o pointer compression)",
+}
+
+
+def ci(stddev, n):
     Z = 2.576  # 99% interval
     return Z * (stddev / math.sqrt(n))
+
 
 def slowdown(baseline, comparison):
     return ((comparison - baseline) / baseline) * 100
 
-def is_statistically_distinguishable(baseline_avg, baseline_ci, comparison_avg, comparison_ci):
-    return not (baseline_avg + baseline_ci < comparison_avg - comparison_ci or 
-                baseline_avg - baseline_ci > comparison_avg + comparison_ci)
+
+def is_statistically_distinguishable(
+    baseline_avg, baseline_ci, comparison_avg, comparison_ci
+):
+    return not (
+        baseline_avg + baseline_ci < comparison_avg - comparison_ci
+        or baseline_avg - baseline_ci > comparison_avg + comparison_ci
+    )
 
 
-with open('speedometer2.1.json', 'r') as file:
-    data = json.load(file)
+infile = sys.argv[1]
+outfile = sys.argv[2]
 
-datafile = sys.argv[1]
+with open(infile, "r") as f:
+    data = json.load(f)
+
 
 # Filter only totals and extract averages and standard deviations
-def extract(datafile):
-    return {k: {'average': v['average'], 'stddev': v['stddev']} 
-            for k, v in data.items() if 'total' in k}
+def extract(data):
+    things_we_care_about = {
+        k: {"average": v["average"], "stddev": v["stddev"]} for k, v in data.items()
+    }
+    return pd.DataFrame.from_dict(things_we_care_about, orient="index")
 
-hdata = data['chrome_v133_chrome-handles']['data']
-ddata = data['chrome_v133_chrome-direct']['data']
-hncdata = data['chrome_v133_chrome-handles-no-compression']['data']
-dncdata = data['chrome_v133_chrome-direct-no-compression']['data']
 
-handles_df = pd.DataFrame.from_dict(extract(hdata), orient='index')
-direct_df = pd.DataFrame.from_dict(extract(ddata), orient='index')
-handles_no_compression_df = pd.DataFrame.from_dict(extract(hncdata), orient='index')
-direct_no_compression_df = pd.DataFrame.from_dict(extract(dncdata), orient='index')
+iters = int(os.environ["ITERS"])
+for v in data.values():
+    assert all([len(story["values"]) == iters for story in v["data"].values()])
 
-assert(len(handles_df) == len(direct_df))
+headers = [v["info"]["binary"] for k, v in sorted(data.items())]
+data = [extract(v["data"]) for k, v in sorted(data.items())]
 
 # Calculate confidence intervals
-iters = len(handles_df)
-handles_df['ci'] = handles_df['stddev'].apply(lambda x: confidence_interval(x, iters))
-direct_df['ci'] = direct_df['stddev'].apply(lambda x: confidence_interval(x, iters))
+for cfg in data:
+    cfg["ci"] = cfg["stddev"].apply(lambda x: ci(x, iters))
+    cfg["display"] = (
+        cfg["average"].round(2).astype(str)
+        + " (\u00B1 "
+        + cfg["ci"].round(3).astype(str)
+        + ")"
+    )
 
-df = pd.DataFrame({
-    'handles_avg': handles_df['average'],
-    'handles_ci': handles_df['ci'],
-    'direct_avg': direct_df['average'],
-    'direct_ci': direct_df['ci']
-})
+merged = {}
+for binpath, results in zip(headers, data):
+    cfg = binpath.split("/")[-2]
+    merged[PRETTY_CFGS[cfg]] = results["display"]
 
-# Add slowdown and statistically distinguishable cols
-df['slowdown'] = df.apply(lambda row: slowdown(row['handles_avg'], row['direct_avg']), axis=1)
-df['statistically_distinguishable'] = df.apply(
-    lambda row: is_statistically_distinguishable(row['handles_avg'], row['handles_ci'], 
-                                                 row['direct_avg'], row['direct_ci']), axis=1
-)
+df = pd.DataFrame(merged)
+df = df.reset_index().rename(columns={"index": "Test Configuration"})
 
-# Formatting
-df.columns = ['Chrome-Handles Average', 'Chrome-Handles 99% CI', 
-              'Chrome-Direct Average', 'Chrome-Direct 99% CI', 
-              'Slowdown (%)', 'Statistically Distinguishable']
-df = df.reset_index().rename(columns={'index': 'Test Configuration'})
-
-outfile = Path(datafile).stem + '.md'
-with open("w", outfile):
-    df.to_markdown(floatfmt=".2f")
+with open(outfile, "w") as f:
+    f.write(df.to_markdown(floatfmt=".2f"))
